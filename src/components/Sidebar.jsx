78de8ae17f27ef5ce0c357310cd4e1b274324180
Gyuri <rbf0311@gmail.com>
2025-02-11 02:56:09 +0900

    feat: Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,36 @@
 import styles from './Sidebar.module.css';
-import { Profile, Home, Report , Setting } from '@assets/icons';
+import { Profile, Home, Report, Setting } from '@assets/icons';
+import { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [selected, setSelected] = useState(() => {
+    const path = location.pathname;
+    if (path === '/') return 'home';
+    if (path === '/report') return 'report';
+    if (path === '/setting') return 'setting';
+    return 'home';
+  });
+
+  const handleMenuClick = (menu) => {
+    setSelected(menu);
+    switch (menu) {
+      case 'home':
+        navigate('/');
+        break;
+      case 'report':
+        navigate('/report');
+        break;
+      case 'setting':
+        navigate('/setting');
+        break;
+      default:
+        navigate('/');
+    }
+  }
+
   return (
     <div className={styles.sidebar}>
       <div className={styles.sidebarHeader}>
@@ -12,21 +41,30 @@ const Sidebar = () => {
         </div>
       </div>
       <div className={styles.sidebarMenu}>
-        <div className={`${styles.menuItem} ${styles.selected}`}>
-          <Home selected={true} />
+        <div 
+          className={`${styles.menuItem} ${selected === 'home' ? styles.selected : ''}`} 
+          onClick={() => handleMenuClick('home')}
+        >
+          <Home selected={selected === 'home'} />
           <p>대시보드</p>
         </div>
-        <div className={styles.menuItem}>
-          <Report />
+        <div 
+          className={`${styles.menuItem} ${selected === 'report' ? styles.selected : ''}`} 
+          onClick={() => handleMenuClick('report')}
+        >
+          <Report selected={selected === 'report'} />
           <p>월별 분석 리포트</p>
         </div>
-        <div className={styles.menuItem}>
-          <Setting />
+        <div 
+          className={`${styles.menuItem} ${selected === 'setting' ? styles.selected : ''}`} 
+          onClick={() => handleMenuClick('setting')}
+        >
+          <Setting selected={selected === 'setting'} />
           <p>설정</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Sidebar;
