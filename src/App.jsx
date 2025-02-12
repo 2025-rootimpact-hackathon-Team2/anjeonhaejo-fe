0fa021d43d1b1e176f4d85fe09d3de20d61c4c14
Gyuri <rbf0311@gmail.com>
2025-02-12 21:08:41 +0900

    style: Implement Modal component UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import UserReportView from './pages/user/UserReportView';
 import Dashboard from './pages/Dashboard';
+import Modal from './components/Modal';
+import { useState } from 'react';
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
   return (
     <>
+      {isModalOpen && <Modal onClose={() => setIsModalOpen(false)} />}
       <Routes>
         <Route element={<Layout />}>
           <Route path="/user" element={<UserReportView />} />
--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,7 @@
 import DangerTriangle from './DangerTriangle.svg?react';
 import WarningTriangle from './WarningTriangle.svg?react';
 import DangerTriangleSmall from './DangerTriangleSmall.svg?react';
+import DangerTriangleBig from './DangerTriangleBig.svg?react';
 import CheckCircleSmall from './CheckCircleSmall.svg?react';
 import Profile from './Profile.svg?react';
 import Home from './Home.jsx';
@@ -11,6 +12,7 @@ export {
   DangerTriangle,
   WarningTriangle,
   DangerTriangleSmall,
+  DangerTriangleBig,
   CheckCircleSmall,
   Profile,
   Home,
