40dc8978359f03939812712a9e2afd2d481bce52
GyunHee <ef3232@naver.com>
2025-02-14 03:33:24 +0900

    deploy: 포트 설정
    포트 설정

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr()
-  ],
+  plugins: [react(), svgr()],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: [
-      { find: "@components", replacement: "/src/components" },
-      { find: "@constants", replacement: "/src/constants" },
-      { find: "@pages", replacement: "/src/pages" },
-      { find: "@assets", replacement: "/src/assets" },
-      { find: "@api", replacement: "/src/api" },
-      { find: "@hooks", replacement: "/src/hooks" },
-      { find: "@store", replacement: "/src/store" },
-      { find: "@", replacement: "/src" },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@constants', replacement: '/src/constants' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@assets', replacement: '/src/assets' },
+      { find: '@api', replacement: '/src/api' },
+      { find: '@hooks', replacement: '/src/hooks' },
+      { find: '@store', replacement: '/src/store' },
+      { find: '@', replacement: '/src' },
     ],
   },
-})
+});
