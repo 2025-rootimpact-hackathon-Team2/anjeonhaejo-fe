35f346a416523c173c50112ad6e6ac787fc77b57
GyunHee <ef3232@naver.com>
2025-02-14 00:43:39 +0900

    feat: 모바일 헤더 추가
    모바일 헤더 추가

--- src/components/mobile/MobileHeader.jsx
+++ src/components/mobile/MobileHeader.jsx
@@ -4,7 +4,7 @@ import styles from './MobileHeader.module.css';
 export default function MobileHeader() {
   return (
     <header className={styles.header}>
-      <p>회사 로고</p>
+      <div></div>
     </header>
   );
 }
